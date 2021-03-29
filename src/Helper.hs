318cb2282a62042c29c5876977d4960067fd78b1
Tom Smeding <tom@tomsmeding.com>
2021-03-29 09:15:54 +0200

    Remove Helper.hs

--- src/Helper.hs
+++
@@ -1,8 +0,0 @@
--- | Different helper functions used in the library
-module Helper where
-
-import           Control.Monad.State.Lazy (State, evalState)
-
--- | Helper function to run one of the AD methods
-runAD :: (t -> State Integer a) -> t -> a
-runAD d term = evalState (d term) 0
