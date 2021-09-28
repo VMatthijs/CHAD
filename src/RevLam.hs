9be233ee0fcbcf2e41ceb35122c5acd6797cecc7
Tom Smeding <tom@tomsmeding.com>
2021-09-28 16:28:27 +0200

    Use Let, not let_

--- src/RevLam.hs
+++ src/RevLam.hs
@@ -57,9 +57,6 @@ onehotEnv :: EnvS env -> Idx env t -> LinLambda env' (Dr2 t) (Dr2Env env)
 onehotEnv (EPush _ env) Z = LinPair (LinZero (dr2envt env)) LinVar
 onehotEnv (EPush t env) (S i) = LinPair (onehotEnv env i) (LinZero (dr2t t))
 
-let_ :: Type a -> Lambda env a -> Lambda (a ': env) b -> Lambda env b
-let_ ty rhs body = App (Lambda ty body) rhs
-
 drt :: EnvS env -> Type t -> Type (Dr1 t, LFun (Dr2 t) (Dr2Env env))
 drt env t = TPair (dr1t t) (TLFun (dr2t t) (dr2envt env))
 
@@ -86,9 +83,9 @@ dr env = \case
     let resty = typeof body
         bodydr2ty = drt (EPush argty env) (typeof body)
         bndty = TFun (dr1t argty) bodydr2ty
-    in let_ bndty (Lambda (dr1t argty) $ dr (EPush argty env) body) $
+    in Let (Lambda (dr1t argty) $ dr (EPush argty env) body) $
          Pair (Lambda (dr1t argty) $
-                 let_ bodydr2ty (Var bndty (S Z) `App` Var (dr1t argty) Z) $
+                 Let (Var bndty (S Z) `App` Var (dr1t argty) Z) $
                    Pair (Fst (Var bodydr2ty Z))
                         (makeLFunTerm (dr2t resty) $
                            LinSnd (Snd (Var bodydr2ty Z) `LinApp` LinVar)))
@@ -104,8 +101,8 @@ dr env = \case
         resty = typeof body
         bndty1 = drt env rhsty
         bndty2 = drt (EPush rhsty env) resty
-    in let_ bndty1 (dr env rhs) $
-       let_ bndty2 (substLam (wSucc wId) (Fst (Var (drt env rhsty) Z))
+    in Let (dr env rhs) $
+       Let (substLam (wSucc wId) (Fst (Var (drt env rhsty) Z))
                         (dr (EPush rhsty env) body)) $
          Pair (Fst (Var bndty2 Z))
               (makeLFunTerm (dr2t resty) $
@@ -119,9 +116,9 @@ dr env = \case
         argdr2ty = drt env (typeof arg)
         fundr2ty = drt env (typeof fun)
         fundiffty = TPair (dr1t resty) (TLFun (dr2t resty) (dr2t argty))
-    in let_ argdr2ty (dr env arg) $
-       let_ fundr2ty (sinkLam (wSucc wId) (dr env fun)) $
-       let_ fundiffty (App (Fst (Var fundr2ty Z)) (Fst (Var argdr2ty (S Z)))) $
+    in Let (dr env arg) $
+       Let (sinkLam (wSucc wId) (dr env fun)) $
+       Let (App (Fst (Var fundr2ty Z)) (Fst (Var argdr2ty (S Z)))) $
          Pair (Fst (Var fundiffty Z))
               (makeLFunTerm (dr2t resty) $
                  LinPlus (Snd (Var argdr2ty (S (S Z)))
@@ -135,8 +132,8 @@ dr env = \case
     let e1dr2ty = drt env (typeof e1)
         e2dr2ty = drt env (typeof e2)
         adjty = TPair (dr2t (typeof e1)) (dr2t (typeof e2))
-    in let_ e1dr2ty (dr env e1) $
-       let_ e2dr2ty (sinkLam (wSucc wId) (dr env e2)) $
+    in Let (dr env e1) $
+       Let (sinkLam (wSucc wId) (dr env e2)) $
          Pair (Pair (Fst (Var e1dr2ty (S Z))) (Fst (Var e2dr2ty Z)))
               (makeLFunTerm adjty $
                  LinPlus (Snd (Var e1dr2ty (S Z)) `LinApp` LinFst LinVar)
@@ -145,7 +142,7 @@ dr env = \case
   Fst e ->
     let TPair t1 t2 = typeof e
         dty = drt env (typeof e)
-    in let_ dty (dr env e) $
+    in Let (dr env e) $
          Pair (Fst (Fst (Var dty Z)))
               (makeLFunTerm (dr2t t1) $
                  Snd (Var dty Z)
@@ -154,7 +151,7 @@ dr env = \case
   Snd e ->
     let TPair t1 t2 = typeof e
         dty = drt env (typeof e)
-    in let_ dty (dr env e) $
+    in Let (dr env e) $
          Pair (Snd (Fst (Var dty Z)))
               (makeLFunTerm (dr2t t2) $
                  Snd (Var dty Z)
@@ -164,7 +161,7 @@ dr env = \case
     | let dty = drt env (typeof arg)
     , Just Refl <- geq (typeof arg) (dr1t (typeof arg))
     , Just Refl <- geq resty (dr1t resty)
-    -> let_ dty (dr env arg) $
+    -> Let (dr env arg) $
          Pair (Op resty op (Fst (Var dty Z)))
               (makeLFunTerm (dr2t resty) $
                  let dop = drOp (typeof arg) resty op `App` Fst (Var dty Z)
