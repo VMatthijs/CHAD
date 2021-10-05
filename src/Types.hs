e2582e3160a7da3e31d578ea97f0fe0e5a709f5e
Tom Smeding <tom@tomsmeding.com>
2021-10-05 15:14:37 +0200

    Purge coproducts (hopefully temporarily!)

--- src/Types.hs
+++ src/Types.hs
@@ -45,16 +45,6 @@ module Types
   , lCopowFold
   , lZip
   , lMap
-  , dFoldr
-  , dtFoldr
-  , dIt
-  , dtIt
-  , lRec
-  , lIt
-  , LEither
-  , lInl
-  , lInr
-  , lCoPair
   , Copower
   , singleton
   , Df1
@@ -70,10 +60,8 @@ module Types
   ) where
 
 import           Data.Kind                 (Constraint)
-import qualified Data.Vector.Unboxed.Sized as V (Unbox, Vector, foldr, init,
-                                                 last, map, prescanr, replicate,
-                                                 scanl, sum, toList, zip,
-                                                 zipWith)
+import qualified Data.Vector.Unboxed.Sized as V (Vector, map, replicate,
+                                                 sum, toList, zip, zipWith)
 import           GHC.TypeNats              (KnownNat)
 
 -- | Real scalars
@@ -90,27 +78,9 @@ newtype Copower a b =
 newtype LFun a b =
   MkLFun (a -> b)
 
--- | Linear coproduct
-newtype LEither a b =
-  MkLEither (Maybe (Either a b))
-
 -- Methods for copowers
 singleton :: LT b => a -> LFun b (Copower a b)
 singleton t = MkLFun $ \x -> MkCopow [(t, x)]
-
--- Methods for linear coproducts
-lInl :: (LT a, LT b) => LFun a (LEither a b)
-lInl = MkLFun (MkLEither . Just . Left)
-
-lInr :: (LT a, LT b) => LFun b (LEither a b)
-lInr = MkLFun (MkLEither . Just . Right)
-
-lCoPair :: (LT a, LT b, LT c) => LFun a c -> LFun b c -> LFun (LEither a b) c
-lCoPair (MkLFun f) (MkLFun g) = MkLFun h
-  where
-    h (MkLEither Nothing)          = zero
-    h (MkLEither (Just (Left a)))  = f a
-    h (MkLEither (Just (Right b))) = g b
 
 -- Methods for linear functions
 lId :: LT a => LFun a a
@@ -201,94 +171,6 @@ lPlus (MkLFun f) (MkLFun g) = MkLFun $ \x -> plus (f x) (g x)
 lMap :: KnownNat n => Vect n -> LFun (Scal -> Scal) (Vect n)
 lMap x = MkLFun $ \g -> V.map g x
 
-dFoldr ::
-     (KnownNat n, V.Unbox a, V.Unbox b, LT b)
-  => ((Scal, a) -> (a, LFun (Scal, b) b))
-  -> a
-  -> Vect n
-  -> LFun (((Scal, a) -> b, b), Vect n) b
-dFoldr f i v =
-  MkLFun $ \((f', i'), v') ->
-    let s = V.prescanr (curry (fst . f)) i v
-        vvps = V.zip v (V.zip v' s)
-        g (vi, (vpi, si)) acc =
-          f' (vi, si) `plus` lApp (snd (f (vi, si))) (vpi, acc)
-     in V.foldr g i' vvps
-
-dtFoldr ::
-     (V.Unbox a, V.Unbox b, LT b)
-  => ((Scal, a) -> (a, LFun b (Scal, b)))
-  -> a
-  -> Vect n
-  -> LFun b ((Copower (Scal, a) b, b), Vect n)
-dtFoldr f i v =
-  MkLFun $ \w ->
-    let s = V.prescanr (curry (fst . f)) i v
-        vs = V.zip v s
-        svs = V.scanl (flip $ curry (snd . uncurry (lApp . snd . f))) w vs
-        vssvs = V.zip vs (V.init svs)
-     in ( (MkCopow (V.toList vssvs), V.last svs)
-        , V.map (fst . uncurry (lApp . snd . f)) vssvs)
-
-dIt ::
-     (LT d2a, LT d2b, LT d2c)
-  => ((d1a, d1b) -> Either d1c d1b)
-  -> ((d1a, d1b) -> LFun (d2a, d2b) (LEither d2c d2b))
-  -> ((d1a, d1b) -> LFun (d2a, d2b) d2c)
-dIt d1t d2t (d1a, d1b) =
-  MkLFun $ \(d2a, d2b) ->
-    let d1bs = scanIt d1t (d1a, d1b)
-        vfst (MkLEither Nothing)          = zero
-        vfst (MkLEither (Just (Left a)))  = a
-        vfst (MkLEither (Just (Right _))) = error "This should never happen."
-        vsnd (MkLEither Nothing)          = zero
-        vsnd (MkLEither (Just (Left _)))  = error "This should never happen."
-        vsnd (MkLEither (Just (Right b))) = b
-     in vfst
-          (d2t (d1a, last d1bs) `lApp`
-           ( d2a
-           , foldl
-               (\acc d1b' -> vsnd (d2t (d1a, d1b') `lApp` (d2a, acc)))
-               d2b
-               (init d1bs)))
-
-dtIt ::
-     (LT d2a, LT d2b, LT d2c)
-  => ((d1a, d1b) -> Either d1c d1b)
-  -> ((d1a, d1b) -> LFun (LEither d2c d2b) (d2a, d2b))
-  -> ((d1a, d1b) -> LFun d2c (d2a, d2b))
-dtIt d1t d2t (d1a, d1b) =
-  MkLFun $ \d2c ->
-    let d1bs = scanIt d1t (d1a, d1b)
-        d2ad2b = d2t (d1a, last d1bs) `lApp` (lInl `lApp` d2c)
-        d2ad2bs =
-          scanr
-            (\d1b' acc -> d2t (d1a, d1b') `lApp` (lInr `lApp` snd acc))
-            d2ad2b
-            (init d1bs)
-     in (foldr plus zero (map fst d2ad2bs), snd (head d2ad2bs))
-
-scanIt :: ((c, a) -> Either b a) -> (c, a) -> [a]
-scanIt f (c, a) =
-  case f (c, a) of
-    Left _ -> [a]
-    Right a' ->
-      let as = scanIt f (c, a')
-       in a : as
-
-lRec :: (LT a, LT b) => LFun (a, b) b -> LFun a b
-lRec (MkLFun g) = MkLFun $ lrec g
-  where
-    lrec f a = f (a, lrec f a)
-
-lIt :: (LT a, LT b, DZ b) => LFun b (a, b) -> LFun b a
-lIt f =
-  MkLFun $ \b ->
-    if isZero b
-      then zero
-      else let (a, b') = f `lApp` b
-            in plus a (lIt f `lApp` b')
-
 -- Forward mode AD type families
 type family Df1 a = r | r -> a where
   Df1 Scal = Scal
@@ -296,7 +178,6 @@ type family Df1 a = r | r -> a where
   Df1 (a -> b) = Df1 a -> (Df1 b, LFun (Df2 a) (Df2 b))
   Df1 (a, b) = (Df1 a, Df1 b)
   Df1 () = ()
-  Df1 (Either a b) = Either (Df1 a) (Df1 b)
 
 type family Df2 a where
   Df2 Scal = Scal
@@ -304,7 +185,6 @@ type family Df2 a where
   Df2 (a -> b) = Df1 a -> Df2 b
   Df2 (a, b) = (Df2 a, Df2 b)
   Df2 () = ()
-  Df2 (Either a b) = LEither (Df2 a) (Df2 b) -- TODO: better to work with Either (Dr2 a) (Dr2 b), but that requires dynamic types or explicit retainment of primals.
 
 -- Reverse mode AD type families
 type family Dr1 a = r | r -> a where
@@ -312,7 +192,6 @@ type family Dr1 a = r | r -> a where
   Dr1 (Vect n) = Vect n
   Dr1 (a, b) = (Dr1 a, Dr1 b)
   Dr1 () = ()
-  Dr1 (Either a b) = Either (Dr1 a) (Dr1 b)
   Dr1 (a -> b) = Dr1 a -> (Dr1 b, LFun (Dr2 b) (Dr2 a))
 
 type family Dr2 a where
@@ -320,7 +199,6 @@ type family Dr2 a where
   Dr2 (Vect n) = Vect n
   Dr2 (a, b) = (Dr2 a, Dr2 b)
   Dr2 () = ()
-  Dr2 (Either a b) = LEither (Dr2 a) (Dr2 b) -- TODO: better to work with Either (Dr2 a) (Dr2 b), but that requires dynamic types or explicit retainment of primals.
   Dr2 (a -> b) = Copower (Dr1 a) (Dr2 b)
 
 -- | "Linear types": types with the structure of a symmetric monoid under
@@ -388,18 +266,6 @@ instance (LT a, LT b) => LT (LFun a b) where
   zero = MkLFun (const zero)
   plus = lPlus
 
-type instance LTctx (LEither a b) = (LT a, LT b)
-
-instance (LT a, LT b) => LT (LEither a b) where
-  zero = MkLEither Nothing
-  plus (MkLEither Nothing) b = b
-  plus a (MkLEither Nothing) = a
-  plus (MkLEither (Just (Left a))) (MkLEither (Just (Left a'))) =
-    MkLEither (Just (Left (a `plus` a')))
-  plus (MkLEither (Just (Right b))) (MkLEither (Just (Right b'))) =
-    MkLEither (Just (Right (b `plus` b')))
-  plus _ _ = error "This should never be used." -- This doesn't make sense.
-
 type instance LTctx [a] = ()
 
 instance LT [a] where
@@ -429,11 +295,6 @@ instance KnownNat n => DZ (Vect n) where
 
 instance DZ b => DZ (Copower a b) where
   isZero (MkCopow xs) = all isZero (map snd xs)
-
-instance (DZ a, DZ b) => DZ (LEither a b) where
-  isZero (MkLEither Nothing)          = True
-  isZero (MkLEither (Just (Left a)))  = isZero a
-  isZero (MkLEither (Just (Right b))) = isZero b
 
 -- | Convenience constraint set that requires 'LT' on the types of (co)tangents
 type LT2 a = (LT (Df2 a), LT (Dr2 a))
